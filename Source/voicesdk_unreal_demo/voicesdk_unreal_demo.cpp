2897fd0fdbd708b981c1fc45b21dc7637f79aff7
IanSlatteryMeta <146737714+IanSlatteryMeta@users.noreply.github.com>
2024-01-19 12:25:07 -0800

    Add composer sample
    Add an example showcasing composer functionality

---
+++ Source/voicesdk_unreal_demo/voicesdk_unreal_demo.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "voicesdk_unreal_demo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, voicesdk_unreal_demo, "voicesdk_unreal_demo" );
