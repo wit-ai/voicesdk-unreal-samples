1532500bb1269df2c8d35ee8d40249dfed7bacec
IanSlatteryMeta <146737714+IanSlatteryMeta@users.noreply.github.com>
2024-01-22 06:32:13 -0800

    Remove unnecessary files

--- Source/voicesdk_unreal_demo/voicesdk_unreal_demo.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "voicesdk_unreal_demo.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, voicesdk_unreal_demo, "voicesdk_unreal_demo" );
